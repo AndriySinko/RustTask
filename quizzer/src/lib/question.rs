e626e4040a72058780656d99736929535c7139ff
TymofiiKotliar <xkotliar@stuba.sk>
2026-03-18 21:01:19 +0100

    update structures question and answer

--- quizzer/src/lib/question.rs
+++ quizzer/src/lib/question.rs
@@ -1,9 +1,5 @@
 pub struct Question {
-    qustion: String,
-    answers: Vec<Answer>,
-}
-
-pub struct Answer {
-    answer: String,
-    is_correct: bool,
+    question: String,
+    answer: Vec<String>,
+    correct_answer: usize,
 }
